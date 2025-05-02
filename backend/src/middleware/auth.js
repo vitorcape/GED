7fa7cfdee4212709db596631046efc0cea24b2d2
Vitor Capelli <vitinhocapelli18@gmail.com>
2025-05-02 00:50:14 -0300

    Corrige middleware para permitir preflight CORS

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,15 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
+    // Ignora preflight
+    if (req.method === 'OPTIONS') return next();
+
     const token = req.header('Authorization')?.split(" ")[1];
 
     if (!token) return res.status(401).json({ erro: 'Acesso negado' });
 
     try {
+        const jwt = require('jsonwebtoken');
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.usuario = decoded;
         next();
     } catch (err) {
         res.status(400).json({ erro: 'Token inválido' });
     }
-};
+};  
